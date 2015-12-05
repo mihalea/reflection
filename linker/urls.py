53a90aa604466ad83b32fe99fb7182545f9fab14
Mircea Mihalea <mircea@mihalea.ro>
2015-12-05 18:05:26 +0200

    From the git index links are available to the details for each project

--- linker/urls.py
+++ linker/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^show/(?P<id>[0-9]+)', views.show, name="edit"),
+    url(r'^(?P<id>[0-9]+)', views.show, name="show"),
     url(r'^', views.index, name="index"),
 ]
