f19bc7c7151706fefe71ac16b5d7bd7d38224ce8
Mircea Mihalea <mircea@mihalea.ro>
2015-12-16 15:44:29 +0200

    Added restrictions to the administrative pages

--- linker/utils/update.py
+++ linker/utils/update.py
@@ -4,10 +4,12 @@ import logging
 
 from linker.models import Project
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.contrib.auth.decorators import user_passes_test
 
 API_URL = "https://api.github.com/"
 log = logging.getLogger(__name__)
 
+@user_passes_test(is_staff)
 def add(username, repository):
 	try:
 		project = Project.objects.get(username=username, repository=repository)
@@ -34,6 +36,7 @@ def add(username, repository):
 		log.error("Multiple projects with the same pair returned. This should" +
 			"not happen")
 
+@user_passes_test(is_staff)
 def update(username, repository):
 	try:
 		project = Project.objects.get(username=username, repository=repository)
@@ -50,6 +53,7 @@ def update(username, repository):
 		log.warn("Project is not in the database and can't be updated " +
 			username + ":" + repository)
 
+@user_passes_test(is_staff)
 def get_metadata(username, repository):
 	url = "{}repos/{}/{}/readme".format(API_URL, username, repository)
 	headers = {'User-Agent': 'Mihalea/Reflection'}
@@ -64,3 +68,6 @@ def get_metadata(username, repository):
 
 	readme = requests.get(content['download_url'], headers).text
 	return sha, readme
+
+def is_staff(user):
+	return user.is_staff
