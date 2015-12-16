a7a337546fdcb4b9df8fb4764bc1b76dfc375493
Mircea Mihalea <mircea@mihalea.ro>
2015-12-16 14:51:05 +0200

    Added logging to the project

--- linker/utils/update.py
+++ linker/utils/update.py
@@ -1,10 +1,12 @@
 import requests
 import json
+import logging
 
 from linker.models import Project
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 API_URL = "https://api.github.com/"
+log = logging.getLogger(__name__)
 
 def add(username, repository):
 	try:
@@ -13,12 +15,16 @@ def add(username, repository):
 		print("Project already exists!")
 	except ObjectDoesNotExist:
 		#Project does not exist already and it can be added
-		sha, readme = get_metadata(username, repository)
-		Project.objects.create(
-			sha = sha,
-			username = username,
-			repository = repository,
-			readme = readme)
+		try:
+			sha, readme = get_metadata(username, repository)
+			Project.objects.create(
+				sha = sha,
+				username = username,
+				repository = repository,
+				readme = readme)
+		except ValueError as err:
+			log.info("The provided username:repository could not be found")
+
 	except MultipleObjectsReturned:
 		#An error has ocurred and duplicates have been generated.
 		#Further inspection needed
@@ -29,6 +35,9 @@ def get_metadata(username, repository):
 	headers = {'User-Agent': 'Mihalea/Reflection'}
 	response = requests.get(url, headers)
 	content = json.loads(response.text)
+
+	if not 'sha' in content:
+		raise ValueError("The pair username:repository could not be found")
 
 	sha = content['sha']
 	readme = requests.get(content['download_url'], headers).text
