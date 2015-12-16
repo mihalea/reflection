45c2cca3da5d2e4fba41e79061621c600f9074a4
Mircea Mihalea <mircea@mihalea.ro>
2015-12-16 12:46:21 +0200

    Modified the models to fit the new architecture

--- linker/models.py
+++ linker/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Project (models.Model):
-    origin = models.URLField()
-    latest_sha = models.CharField(max_length=64)
+    sha = models.CharField(max_length=64)
     title = models.CharField(max_length=64)
     content = models.TextField()
+
+class Settings (models.Model):
+    default_username = models.CharField(max_length=64)
