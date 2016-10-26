b3366a764457220e4189bf4784351a0d2971fbe1
Mircea Mihalea <mircea@mihalea.ro>
2016-10-26 13:53:58 -0400

    Moved the SECRET_KEY to a python module

--- reflection/settings.py
+++ reflection/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from . import secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/opt/reflection/key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = secret.KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -185,10 +185,3 @@ LINKS = {
     "facebook": "http://facebook.com/mihalea.m",
 }
 
-X_FRAME_OPTIONS = 'DENY'
-CSRF_COOKIE_SECURE = True
-CSRF_COOKIE_HTTPONLY = True
-SESSION_COOKIE_SECURE = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = True
