0064cefcf48e9cfc8ad5c8d3799cb77e5f50008e
Mircea Mihalea <mircea@mihalea.ro>
2017-06-17 18:59:58 +0300

    Add environment file for docker

--- web/reflection/settings.py
+++ web/reflection/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-from . import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.secret_key
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not config.production
+DEBUG = os.getenv("MODE", "debug") == "debug"
 
 ALLOWED_HOSTS = ['www.mihalea.ro', 'mihalea.ro']
 
