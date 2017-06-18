55a29adcf493d961223adaaba320c4962ad1665c
Mircea Mihalea <mircea@mihalea.ro>
2017-06-18 14:17:37 +0300

    Add SSL using letsencrypt

--- config/start.sh
+++ config/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo certbot certonly --webroot --webroot-path=/opt/reflection/data/webroot -d mihalea.ro -d www.mihalea.ro
+sudo certbot certonly --webroot --webroot-path=/opt/reflection/data/webroot -d mihalea.ro -d www.mihalea.ro 
 sudo openssl dhparam -out /etc/letsencrypt/live/mihalea.ro/dhparam.pem 2048
-
-15 3 19 * *  /usr/bin/certbot renew --quiet --renew-hook "cd /opt/reflection && docker-compose restart"
+echo -e "ADD TO SUDO CRONTAB:
+15 3 19 * *  /opt/reflection/config/renew.sh" 
